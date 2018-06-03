398d8f4acc237bbd9e3b61a39a1320cdd94f25ff
Kamen <k.todorov@ultraplay.co>
2018-06-03 22:52:05 +0300

    added form for creating a task

---
+++ src/components/tasks/create-task-form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CreateTaskForm extends React.Component {
+    render() {
+        return (
+            <form>
+                Form for creating a task
+            </form>
+        );
+    }
+}
+
+export default CreateTaskForm;
