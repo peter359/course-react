f63ac2c4393e263b22596d9dfb90ba9f6a163286
Kamen <k.todorov@ultraplay.co>
2018-06-20 23:31:28 +0300

    refactored the project structure; added backend configuration

---
+++ client/src/components/members/view-members.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Route, Link, withRouter } from 'react-router-dom';
+import Member from './member';
+import AddMemberForm from './add-member-form';
+
+class ViewMembers extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Members of {this.props.project.name}</h1>
+                {this.props.members.map(m => <Member key={m.id} {...this.props} member={m} onMemberRemove={this.props.onMemberRemove}/>)}
+                <Link className="btn btn-primary" to="/project/members/add_member">Add Member</Link>
+
+                <Route path="/project/members/add_member" render={props => <AddMemberForm {...this.props} />} />
+            </div>
+        );
+    }
+}
+
+export default ViewMembers;
--- src/components/register/newid.js
+++
@@ -1,5 +0,0 @@
-let lastId = 1;
-
-export default function() {
-    return lastId++;
-}
